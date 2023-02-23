dd8ec069a3030a24e4327ce7d3511f192e972afe
david-auk <david@aukes.com>
2023-02-23 20:04:18 +0100

    created last check for main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import requests
 
 # Checking and writing Telegram API token
 if os.path.isfile("./telegram-token.txt") is False:
-	print("Telegram token not found..\n\nWrite N/A for none.")
-	token = input("Telegram Token: ")
+	print("Telegram token not found..\n\nN/A for none.")
+	token = input("Telegram Token:\t\t")
 	if token == "N/A":
 		open("./telegram-token.txt", 'a').close()
 	else:
@@ -13,19 +13,42 @@ if os.path.isfile("./telegram-token.txt") is False:
 		tgFile.write(token)
 		tgFile.close
 
+# Checking and writing user(s) ChatID
+if os.path.isfile("./user-token.txt") is False:
+	print("User ChatID not found..\n\nN/A for none.")
+	hostChatId = input("Host ChatID:\t\t")
+	if hostChatId == "N/A":
+		open("./user-token.txt", 'a').close()
+	else:	
+		print("Seperate with ',' (enter for none)")
+		aditionalChatId = input("Aditional ChatID:\t")
+		if aditionalChatId:
+			aditionalChatId = aditionalChatId.replace(",", "\n")
+			chatIdTotaal = hostChatId + "\n" + aditionalChatId
+		else:
+			chatIdTotaal = hostChatId
+
+		chatIdFile = open("./user-token.txt", "w")
+		chatIdFile.write(chatIdTotaal)
+		chatIdFile.close
+
 # Function for messaging host using query
 def msgHost(query):
 	telegramToken = open("telegram-token.txt").read()
 	formatedQuote = urllib.parse.quote(query)
-	hostToken = open("user-token.txt").readline().strip('\n')
-	requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(telegramToken,hostToken,formatedQuote))
+	hostChatId = open("user-token.txt").readline().strip('\n')
+	requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(telegramToken,hostChatId,formatedQuote))
 
 # Function for messaging all using query
 def msgAll(query):
 	telegramToken = open("telegram-token.txt").read()
 	formatedQuote = urllib.parse.quote(query)
-	usersToken = open("user-token.txt").read()
-	with open('user-token.txt') as usersToken:
-		for currentUserToken in usersToken:
-			currentUserToken = currentUserToken.strip()
-			requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(telegramToken,currentUserToken,formatedQuote))
+	usersChatId = open("user-token.txt").read()
+	with open('user-token.txt') as usersChatId:
+		for currentUserChatId in usersChatId:
+			currentUserChatId = currentUserChatId.strip()
+			requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(telegramToken,currentUserChatId,formatedQuote))
+
+# Function for searching DB
+
+# Function for adding to DB
