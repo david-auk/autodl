0b0ca947ae0337a0635d0190d8bd31a5547f5bd0
david-auk <david@aukes.com>
2023-02-23 17:17:21 +0100

    made functions and checks for main.py

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import os
 import urllib.parse
 import requests
 
-query = "Dit is een test!"
+# Checking and writing Telegram API token
+if os.path.isfile("./telegram-token.txt") is False:
+	print("Telegram token not found..\n\nWrite N/A for none.")
+	token = input("Telegram Token: ")
+	if token == "N/A":
+		open("./telegram-token.txt", 'a').close()
+	else:
+		tgFile = open("./telegram-token.txt", "w")
+		tgFile.write(token)
+		tgFile.close
 
-telegramToken = open("telegram-token.txt").read()
-usersToken = open("user-token.txt").read()
-formatedQuote = urllib.parse.quote(query)
+# Function for messaging host using query
+def msgHost(query):
+	telegramToken = open("telegram-token.txt").read()
+	formatedQuote = urllib.parse.quote(query)
+	hostToken = open("user-token.txt").readline().strip('\n')
+	requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(telegramToken,hostToken,formatedQuote))
 
-requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(telegramToken,usersToken,formatedQuote))
+# Function for messaging all using query
+def msgAll(query):
+	telegramToken = open("telegram-token.txt").read()
+	formatedQuote = urllib.parse.quote(query)
+	usersToken = open("user-token.txt").read()
+	with open('user-token.txt') as usersToken:
+		for currentUserToken in usersToken:
+			currentUserToken = currentUserToken.strip()
+			requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(telegramToken,currentUserToken,formatedQuote))
