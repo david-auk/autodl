dc40ede364a6a2db0e2cbbb402695083188960ac
david-auk <david@aukes.com>
2023-02-27 18:45:02 +0100

    adapted to new format of structure from functions.py

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import scrapetube
 import datetime
 import functions
 
-functions.getData("account","id","ALL", "accountList")
+#functions.delData("content", "tYO5OWlihgo")
+#quit()
+
+requestuser = "scanner"
+
+functions.getData("account", "id", 'ALL')
 myCursorChannelRequest = functions.getDataCursor
 for (channelTitle, id, priority) in myCursorChannelRequest:
 
 	priorityColor = functions.colourPriority(priority)
-	print(f"{priorityColor}•{functions.colours['reset']} {channelTitle}:")
+	print(f"{priorityColor}•{functions.colours['reset']} {functions.coloursB['white']}{channelTitle}:{functions.colours['reset']}")
 
 	videos = scrapetube.get_channel(id)	
 
@@ -17,17 +22,25 @@ for (channelTitle, id, priority) in myCursorChannelRequest:
 		urlid = video['videoId']
 		videoTitle = video['title']['runs'][0]['text']
 		print(videoTitle + "\n")
-		functions.getData("content", "id", urlid, "contentCheck")
-		if functions.entryExists:
+		#functions.getData("content", "id", urlid, "contentCheck")
+		entryExists = functions.getDataContentCheck(urlid)
+		if entryExists:
+			# Printing 'Succes' status for if the entry exists
 			print(f"[{functions.coloursB['green']}√{functions.colours['reset']}] https://www.youtube.com/watch?v={urlid}\n")
-
 			break #to next account
 		else:
+			# Printing 'Failed' status for if the entry exists
 			print(f"[{functions.coloursB['red']}X{functions.colours['reset']}] https://www.youtube.com/watch?v={urlid}")
+			
+			# Getting date
 			currentDate = datetime.datetime.now()
 			formattedDate = currentDate.strftime("%d-%m-%Y")
 
-			# functions.addContentData(videoTitle,channelTitle,urlid,"path","thumbnailPath",0,"N/A",formattedDate)
+			# Making the path filename friendly
+			filename = functions.filenameFriendly(videoTitle)
+			print(filename)
+			quit()
+			functions.addContentData(videoTitle,channelTitle,urlid,filename,"thumbnailPath",0,"N/A", requestuser,formattedDate)
 
 			# Download file here
 
